03bb724a393a27b3efc159d3964a06f3b080f7ac
Christopher Park <tuj80083@chinook.cis.temple.edu>
2019-10-23 08:13:14 -0400

    added graphing functionality. Testing to see if worked

--- automation.c
+++ automation.c
@@ -7,21 +7,59 @@
 //time outputs will be stored in Data/ folder.
 int main(){
 
+  FILE *dataNode;
+  FILE *graphData;
+  
   //list of commands used in automation
+  char pull_data[100];//full line pulled with fgets()
+  char* time_dataBasic;//tokenized string of just real time
+  char* time_dataSIMD;//tokenized string of just real time
+  char insertCMD[100];
+  char fileName[100];
+  char graphMakeCMD[100];//command to generate graph
   char basic_op[100];
   char simd_op[100];
 
   printf("starting automated runs\n");
   system("mkdir Data");
+  system("touch Data/GraphData.txt");
 
-  for(int i = 0; i < 1001; i+= 50){//change increment and max vals for graph changes
-     sprintf(basic_op, "time ./nParammult %d > Data/basicOp%d.txt 2>&1", i, i);
+  sprintf(graphMakeCMD, "gnuplot graph.gnu");
+  graphData = fopen("Data/GraphData.txt", "a");
+
+  for(int i = 0; i <501 ; i+= 100){//change increment and max vals for graph changes
+     sprintf(basic_op, "time -p ./nParammult %d > Data/basicOp%d.txt 2>&1", i, i);
      printf("running basicOp n = %d\n", i);
      system(basic_op);
+     
+     //extracting data from operation
+     sprintf(fileName, "Data/basicOp%d.txt", i);
+     dataNode = fopen(fileName, "r");
+     fgets(pull_data, 10, dataNode);
+     fclose(dataNode);
+     //printf("%s\n", pull_data);
+     time_dataBasic = strtok(pull_data, " "); //gets token "real"
+     time_dataBasic = strtok(NULL, " "); //gets token time value
+     //printf("%s\n", time_data);
+     
 
-     sprintf(simd_op, "time ./SIMDmult %d > Data/simdOp%d.txt 2>&1", i, i);
+     sprintf(simd_op, "time -p ./SIMDmult %d > Data/simdOp%d.txt 2>&1", i, i);
      printf("running simdOp n = %d\n", i);
      system(simd_op);
+
+     //extracting data from operation
+     sprintf(fileName, "Data/simdOp%d.txt", i);
+     dataNode = fopen(fileName, "r");
+     fgets(pull_data, 10, dataNode);
+     fclose(dataNode);
+     time_dataSIMD = strtok(pull_data, " "); //gets token "real"
+     time_dataSIMD = strtok(NULL, " "); //gets token time value
+
+     //creating insertCMD
+     sprintf(insertCMD, "%d,     %s,     %s\n", i, time_dataBasic, time_dataSIMD);
+     fprintf(graphData, insertCMD);
   }
+  fclose(graphData);
+  system(graphMakeCMD);
   printf("automated runs complete\n");
 }
---
+++ graph.gnu
@@ -0,0 +1,12 @@
+set terminal png size 800,600
+set output 'Data/graph.png'
+
+#Key means label
+#set key inside bottom right
+
+set xlabel 'Matrix size'
+set ylabel 'Time (sec)'
+set title 'Matrix multiplication speed on Wolfgang Cluster'
+
+plot "Data/GraphData.txt" using 1:2 title 'non-parallel' with linespoint,
+"Data/GraphData.txt" using 1:3 title 'SIMD' with linespoint
