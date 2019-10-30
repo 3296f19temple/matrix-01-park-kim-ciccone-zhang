e6674e7a7a19f997772aaa5e481d4e5ac0f02c74
Chizhang2019 <54897894+Chizhang2019@users.noreply.github.com>
2019-10-30 10:15:00 -0400

    Update mmult.c

--- mmult.c
+++ mmult.c
@@ -102,4 +102,64 @@ void compare_matrices(double* a, double* b, int nRows, int nCols) {
 	}
 	printf("Matrices are the same\n");
 }
+//for mpi test
+
+int mmult_std(double *c,
+              double *a, int aRows, int aCols,
+              double *b, int bRows, int bCols) {
+    
+    //A standard version of the matrix multiplication algorithm to use for control data
+    
+    int i, j, k, N;
+    N = aRows;
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            c[i*bCols + j] = 0;
+            for (k = 0; k < N; k++) {
+                c[i*aCols + j] += a[i*aCols + k] * b[k*bCols + j];
+            }
+        }
+    }
+    return 0;
+}
+
+double* gen_matrix(int n, int m, FILE *fp) {
+    int i, j;
+    double x;
+    double *a = malloc(sizeof(double) * n * m);
+    char buf[4096];
+    
+    //reset file pointer to start of file
+    rewind(fp);
+    
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            //scan an integer from the file and place it into the matrix
+            if(!(fscanf(fp, "%lf", &x))) {
+                fprintf(stderr, "Error reading file while generating matrix");
+                exit(EXIT_FAILURE);
+            }
+            //printf("\t%f", x);
+            a[i*m + j] = x;
+        }
+        //puts("");
+    }
+    return a;
+}
+
+void compare_matrices(double* a, double* b, int nRows, int nCols) {
+	int n = nRows * nCols;
+	int i, j, k;
+	for (k = 0; k < n; ++k) {
+		if (fabs(a[k]-b[k])/fabs(a[k]) > 1e-12) {
+			i = k/nCols;
+			j = k%nCols;
+			printf("a[%d][%d] == %.12g\nb[%d][%d] == %.12g\ndelta == %.12g\nrelerr == %.12g\n",
+				i, j, a[k], i, j, b[k], fabs(a[k]-b[k]), fabs(a[k]-b[k])/fabs(a[k]));
+			return;
+		}
+	}
+	printf("Matrices are the same\n");
+}
+
 
